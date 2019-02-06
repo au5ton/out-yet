ea00511ae182c9b837b72ce031b052fd7a9c990a
Austin Jackson <themacphage@gmail.com>
2019-02-06 05:22:59 +0000

    Data retrieval good

--- app.js
+++ app.js
@@ -2,17 +2,28 @@ const express = require('express')
 const fetch = require('node-fetch')
 require('dotenv').config({ path: process.env.ENF_FILE })
 
+const ombi = require('./lib/ombi')
+const themoviedb = require('./lib/themoviedb')
+
 const port = process.env.NODE_PORT || 3000;
 
 var app = express();
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    fetch('https://api.github.com/users/github')
-    .then(res => res.json())
-    .then(json => {
-        res.json(json)
-    });
+    res.json('Is it out yet?')
+})
+
+router.get('/release_dates', (req, res) => {
+    themoviedb.getReleaseDates(req.query.id).then(data => {
+        res.json(data);
+    })
+})
+
+router.get('/requested_movies', (req, res) => {
+    ombi.getMovies().then(movies => {
+        res.json(movies);
+    })
 })
 
 app.use(process.env.BASEURL, router);
